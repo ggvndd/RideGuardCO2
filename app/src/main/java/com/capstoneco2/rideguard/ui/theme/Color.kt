dcedcb03b123f22098730880c1a4542776e165d8
Gavind <raymtbone@gmail.com>
2025-09-10 13:18:29 +0700

    added theme and color palette

--- app/src/main/java/com/capstoneco2/rideguard/ui/theme/Color.kt
+++ app/src/main/java/com/capstoneco2/rideguard/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.capstoneco2.rideguard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black80 = Color(0xFF000000)
+val Black40 = Color(0xFF313131)
+val Gray40 = Color(0xFF797979)
+val Blue20 = Color(0xFFAFDDFF)
+val Blue40 = Color(0xFF60B5FF)
+val Blue80 = Color(0xFF3674B5)
+val White = Color(0xFFFFFFFF)
+val Green = Color(0xFF06A759)
+val Red = Color(0xFFC50C0C)
--- app/src/main/java/com/capstoneco2/rideguard/ui/theme/Theme.kt
+++ app/src/main/java/com/capstoneco2/rideguard/ui/theme/Theme.kt
@@ -11,26 +11,26 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primary = Blue40,
+    onPrimary = White,
+    secondary = Blue20,
+    onSecondary = Black80,
+    error = Red,
+    onError = White,
+    background = White,
+    onBackground = Black80
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = Blue40,
+    onPrimary = White,
+    secondary = Blue80,
+    onSecondary = White,
+    background = Black40,
+    error = Red,
+    onError = White,
+    onBackground = White
 )
 
 @Composable
